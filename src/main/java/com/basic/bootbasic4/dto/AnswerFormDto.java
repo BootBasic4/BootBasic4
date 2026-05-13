a9544a4da863d3a06476c9ffbbd109700f5689cc
paksak4 <paksak4@naver.com>
2026-05-13 17:55:08 +0900

    feat: dto, controller 코드 작성

--- src/main/java/com/basic/bootbasic4/dto/AnswerFormDto.java
+++ src/main/java/com/basic/bootbasic4/dto/AnswerFormDto.java
@@ -1,4 +1,31 @@
 package com.basic.bootbasic4.dto;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class AnswerFormDto {
+
+    // 등록/조회/수정
+    private String content;
+
+    // 등록/조회
+    private Long questionId;
+    private Long memberId;
+
+    // 조회시에 필요
+    private Long answerId;
+    private String nickname;
+    private String petType;
+    private Integer petStarted;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
 }
