546b4cc9ccbd9eead9ea24aa19811af40234daee
joonho4 <wns4090@gmail.com>
2026-05-14 15:26:03 +0900

    [Feature] question 테이블에 pet_type 컬럼 추가
    - 애완 동물 구분을 위한 pet_type 컬럼을 추가하였습니다

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -22,6 +22,7 @@ CREATE TABLE question (
                           content     TEXT         NOT NULL COMMENT '내용',
                           member_id   BIGINT       NOT NULL COMMENT '작성자(FK)',
                           category    VARCHAR(20)  NOT NULL DEFAULT 'QUESTION' COMMENT '게시판 구분(QUESTION, TIP, FREE 등)',
+                          pet_type     VARCHAR(20)  NOT NULL DEFAULT 'ALL' COMMENT '동물 타입',
                           image_url   VARCHAR(500) NULL     COMMENT '이미지 경로',
                           view_count  INT          NOT NULL DEFAULT 0 COMMENT '조회수',
                           created_at  DATETIME     NOT NULL COMMENT '작성일시',
