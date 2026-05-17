9e51b062c4129f5b570999b38e7241462ef7a0c7
minkyeong <jscbg@naver.com>
2026-05-17 17:11:09 +0900

    Fix: develop merge 충돌 해결 및 validation 추가

--- src/main/java/com/basic/bootbasic4/Service/QuestionService.java
+++ src/main/java/com/basic/bootbasic4/Service/QuestionService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import com.basic.bootbasic4.entity.*;
 
 @Service
 @RequiredArgsConstructor
--- src/main/java/com/basic/bootbasic4/controller/QuestionController.java
+++ src/main/java/com/basic/bootbasic4/controller/QuestionController.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import com.basic.bootbasic4.entity.*;
 
 @Controller
 @RequiredArgsConstructor
