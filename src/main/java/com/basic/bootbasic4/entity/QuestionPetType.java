1a75dd81d2aa47e10f84277b318f4fba585739dc
hohyun <shim9754@gmail.com>
2026-05-20 11:05:19 +0900

    feat: 카테고리 URL 변경 및 UI 한글화

--- src/main/java/com/basic/bootbasic4/entity/QuestionPetType.java
+++ src/main/java/com/basic/bootbasic4/entity/QuestionPetType.java
@@ -1,5 +1,14 @@
 package com.basic.bootbasic4.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum QuestionPetType {
-    ALL, DOG, CAT
+    ALL("동물 전체"),
+    DOG("강아지"),
+    CAT("고양이");
+
+    private final String label;
 }
